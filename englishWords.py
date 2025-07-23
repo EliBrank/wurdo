2599e1fc0ed20b583d3ac0831d371b8cf4abd552
Isaac Edwards <isaactheedwards@gmail.com>
2025-07-23 17:34:22 -0500

    Moving to Laptop for easier use of phonimizer

--- englishWords.py
+++ englishWords.py
@@ -1,12 +1,17 @@
 from english_words import get_english_words_set
+from phonimize import phonimize
+
 
 word_set = get_english_words_set(['web2'], lower=True)
 
 sorted_wordSet = sorted(word_set)
 
-with open("data.json", "w") as f:
+with open("data.csv", "w") as f:
+    f.write("word,\n")
     for word in sorted_wordSet:
-        if len(word) >= 3 & len(word) <= 7:
-            f.writelines(f"{word}\n")
-        else:
-            break
+        if 3 <= len(word) <= 7:
+            f.writelines(f"{word},\n")
+
+with open("data.csv", "r") as baseFile, open("append.txt") as appendFile:
+    baseLines = baseFile.readlines()
+    appendLines = appendFile.readlines()
