47bf3d4c3aac4f217ed3fcc846613fb2dedbb019
Isaac Edwards <isaactheedwards@gmail.com>
2025-07-29 14:48:13 -0500

    Updated the actions.ts and definitons.ts folder. Actions is pretty documented but if there is some confusion check the definitions.ts for an explanation how to do the partial updates. At the moment there only partial updates for one object at a time but there is a construction zone at the bottom of the page for an incomplete batch update function to redis. just needs at type assertion at line 168. Also the testing page is updated for you to visualize the data and what it should look like.

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -2,11 +2,24 @@ export interface WordScore {
     word: string;
     score: number;
 }
-  
-export interface PartialData extends Record<string, any> {
+export interface BatchWordUpdate {
+    word: string;
+    data: PartialUpdateData
+}
+
+export interface FullWordData {
+    pronunciation: string;
+    olo: Record<string, number>;
+    ana: Record<string, number>;
+    rhy:Record<string, number>;
+}
+
+// You can pass this for the scoring system. 
+// Something like updateWordData(word="word", partialData= "oneLetterOff: {"word": "score"}" )
+export interface PartialUpdateData {
     pronunciation?: string;
-    oneLetterOff?: WordScore[];
-    anagram?: WordScore[];
-    rhyme?: WordScore[];
+    oneLetterOff?: Record<string, number>;
+    anagram?: Record<string, number>;
+    rhyme?: Record<string, number>;
   
 }
