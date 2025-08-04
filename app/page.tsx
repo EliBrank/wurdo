5294e887942b7c8713e4494bc7e28b78d122b25b
kelciatkinson <kelciatkinson@gmail.com>
2025-08-04 13:06:18 -0500

    changed anchor tags to buttons for native debugging, buttons have effect like pressing a 3d button

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,16 @@ export default function HomePage() {
     <div>
       <Navbar />
       <main className="container mx-auto p-4">
-        <div className="hidden md:flex space-x-6 font-medium">
+        <div className="hidden md:flex font-medium">
           <p>
             würdo was designed for mobile. Please use a mobile device to enjoy.
             Desktop coming soon!
           </p>
         </div>
-        <section className="md:hidden">
+        <section className="md:hidden flex flex-col items-center py-4">
           <GameModeButton label="Daily" href="/play" />
+          <GameModeButton label="Infinite" href="/play" />
+          <GameModeButton label="Challenge" href="/play" />
         </section>
       </main>
     </div>
--- src/components/GameModeButton.tsx
+++ src/components/GameModeButton.tsx
@@ -7,13 +7,14 @@ type GameModeButtonProps = {
 
 export default function GameModeButton({ label, href }: GameModeButtonProps) {
   const baseClasses =
-    "px-4 py-2 bg-(--green-main) text-white rounded-lg hover:opacity-90 transition";
+    "inline-block text-center px-4 py-2 my-2 min-w-full bg-(--green-main) text-white rounded-lg hover:opacity-90 shadow-[0_4px_0_0_#587F5F] transition-all active:translate-y-1 active:shadow-[0_2px_0_0_#587F5F] active:brightness-95";
 
-  if (href) {
-    return (
-      <a href={href} className={baseClasses}>
-        {label}
-      </a>
-    );
-  }
+  return (
+    <button
+      onClick={() => (href ? (window.location.href = href) : null)}
+      className={baseClasses}
+    >
+      {label}
+    </button>
+  );
 }
