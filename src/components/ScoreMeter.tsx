3896a7162c7db6d7a2a8049a149e6d7df79a193b
kelciatkinson <kelciatkinson@gmail.com>
2025-08-13 15:39:42 -0500

    changed handleSubmit and tried score

--- src/components/ScoreMeter.tsx
+++ src/components/ScoreMeter.tsx
@@ -2,25 +2,18 @@
 import React from "react";
 import { useGameContext } from "@/context";
 
-interface ScoreMeterProps {
-  currentScore: number;
-  maxScore: number;
-}
-
 export default function ScoreMeter() {
-  const { wordScore, totalScore, maxScore, setTotalScore } = useGameContext();
-  if (maxScore === undefined) return 0;
-  if (wordScore === undefined) return 0;
-
-  setTotalScore(Math.round(totalScore + wordScore));
+  const { wordScore, totalScore, setTotalScore } = useGameContext();
+  if (wordScore === undefined) return null;
+  if (totalScore === undefined) return null;
 
   return (
     <div className="w-full">
       <div className="w-full bg-(--navy-main) rounded h-3 flex items-center">
         <div
           className="bg-white h-1.5 transition-all ml-1 duration-300"
-          style={{ width: `${Math.min(percentage, 100)}%` }}
-        />
+          style={{ width: `${Math.min()}%` }}
+        ></div>
       </div>
       <div className="text-sm mt-1 text-center text-black"></div>
     </div>
